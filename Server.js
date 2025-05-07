88e8aaffda456ea544db9d60f747511185156910
Serguz4 <serguz4@gmail.com>
2025-05-07 19:04:37 -0400

    Create Server.js
    const express = require('express');
    const http = require('http');
    const { Server } = require('socket.io');
    const cors = require('cors');
    
    const app = express();
    app.use(cors());
    
    const server = http.createServer(app);
    const io = new Server(server, {
      cors: {
        origin: '*',
        methods: ['GET', 'POST']
      }
    });
    
    io.on('connection', (socket) => {
      console.log('Usuario conectado:', socket.id);
    
      socket.on('send_message', (data) => {
        socket.broadcast.emit('receive_message', data);
      });
    
      socket.on('disconnect', () => {
        console.log('Usuario desconectado:', socket.id);
      });
    });
    
    app.get('/', (req, res) => {
      res.send('GhostChat backend funcionando');
    });
    
    const PORT = process.env.PORT || 3001;
    server.listen(PORT, () => {
      console.log(`Servidor corriendo en puerto ${PORT}`);
    });

---
+++ Server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('Usuario conectado:', socket.id);
+
+  socket.on('send_message', (data) => {
+    socket.broadcast.emit('receive_message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado:', socket.id);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('GhostChat backend funcionando');
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
